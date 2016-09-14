e61612b9ac512310c85fe4f772fe6dff128566ba
Sandeep Maram <sandeepmaaram@gmail.com>
2016-09-14 17:39:31 +0530

    first commit

---
+++ app/src/main/java/com/rbt/filepicker/MainActivity.java
@@ -0,0 +1,21 @@
+package com.rbt.filepicker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(this, DriveActivity.class);
+        startActivity(intent);
+    }
+}
